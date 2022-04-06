11c5d1f6982d97d012c493a3b776a13b8e83cd4f
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2022-04-06 10:07:42 +0700

    Add files via upload

---
+++ 13_searching/public/js/script.js
@@ -0,0 +1,37 @@
+const alamat='http://localhost/php-mvc/13/public';
+
+$(function() {
+
+	$('.tombolTambahData').on('click', function() {	
+		$('#formModalLabel').html('Tambah Data Mahasiswa');
+		$('.modal-footer button[type=submit]').html('Tambah Data');
+				$('#nama').val(null);
+				$('#nrp').val(null);
+				$('#email').val(null);
+				$('#jurusan').val(null);
+	});
+
+	$('.tampilModalUbah').on('click', function() {	
+		$('#formModalLabel').html('Ubah Data Mahasiswa');
+		$('.modal-footer button[type=submit]').html('Ubah Data');
+		$('.modal-body form').attr('action', `${alamat}/mahasiswa/ubah`);
+
+		const id = $(this).data('id');
+
+		$.ajax ({
+			url:`${alamat}/mahasiswa/getubah`,
+			data: {id:id},
+			method: 'post',
+			dataType: 'json',
+			success: function(data) {
+				$('#nama').val(data.nama);
+				$('#nrp').val(data.nrp);
+				$('#email').val(data.email);
+				$('#jurusan').val(data.jurusan);
+				$('#id').val(data.id);
+			}
+		});
+
+	});
+
+});
