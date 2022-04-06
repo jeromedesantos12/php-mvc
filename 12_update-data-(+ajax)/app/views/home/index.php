099f5a0f08b3c85d042fc793e22d51da1716f013
Jeremy Santoso <97815661+jeromedesantos12@users.noreply.github.com>
2022-04-06 10:06:33 +0700

    Add files via upload

---
+++ 12_update-data-(+ajax)/app/views/home/index.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="jumbotron mt-5">
+		<h1 class="display-4">Selamat Datang di Website Saya!</h1>
+		<p class="lead">Halo, nama saya <?= $data['nama']; ?> </p>
+		<hr class="my-4">
+		<p>Selamat datang di website saya yang lain dan masih dalam topik CRUD juga. Web ini merupakan mockup untuk kerangka dasar mvc yang umum diterapkan pada framework-framework yang ada, seperti laravel, ci, react, node, dll.</p>
+		<p class="lead">
+		<a class="btn btn-primary btn-lg" href="<?= BASEURL; ?>/mahasiswa" role="button">Pelajari lebih lanjut</a></p>
+</div>
+</div>
